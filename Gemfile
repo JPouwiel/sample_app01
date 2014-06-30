ee958351575a9474e57e16de0fd00be5f03979d5
Jeroen Pouwiel <jeroen.pouwiel@gmail.com>
2014-06-30 16:38:08 +0200

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ group :development, :test do
   gem 'guard-rspec', '2.5.0'
   gem 'spork-rails', '4.0.0'
   gem 'guard-spork', '1.1.0'
+  gem 'factory_girl_rails', '4.2.0'
 end
 
 group :test do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Barsa Man"
+    email    "barsaman@example.com"
+    password "foobar01"
+    password_confirmation "foobar01"
+  end
+end
